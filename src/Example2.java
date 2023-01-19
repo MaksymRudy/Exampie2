fd3e4fb23aacf908d832bdff6fb11b3021325b6c
MaksymRudy <mail@google.com>
2023-01-19 16:16:03 +0200

    init project

---
+++ src/Example2.java
@@ -0,0 +1,16 @@
+public class Example2 {
+    /* Это еще один короткий пример программы .
+Присвоить исходному файлу имя "Exaiaple2 . java
+*/
+    public static void main(String[] args) {
+        int num; // в этой строке кода объявляется переменная с именем num
+        num = 100; // в этой строке кода переменной num
+        // присваивается значение 100
+        System.out.println("Ето переменная num: " + num);
+
+        num = num * 2;
+
+        System.out.print("Znachtnnia peremennoi num * 2 ravno  ");
+        System.out.println(num);
+    }
+}
